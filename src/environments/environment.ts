30f9524e43cc0ac85a4cb5d17c8d871932fd7d66
Mike Bozhedomov <mike.bozhedomov@gmail.com>
2024-09-26 22:06:53 +0300

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  firebase: {
-    apiKey: "AIzaSyAIDxry0hxegpu6IMeKhB75XM75kM7Hb74",
-    authDomain: "ohmystand.firebaseapp.com",
-    projectId: "ohmystand",
-    storageBucket: "ohmystand.appspot.com",
-    messagingSenderId: "137154397242",
-    appId: "1:137154397242:web:d85c99e2ae42272e4ba103",
-    measurementId: "G-T4778Z0PPC"
-  }
+  firebase: {}
 };
