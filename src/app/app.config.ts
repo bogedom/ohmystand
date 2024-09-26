eca278bb27f901223778a8b5c078f44c4d855965
Mike Bozhedomov <mike.bozhedomov@gmail.com>
2024-09-26 20:54:16 +0300

    added environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,21 +5,14 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideFirebaseApp(() => initializeApp({
-      apiKey: "AIzaSyAIDxry0hxegpu6IMeKhB75XM75kM7Hb74",
-      authDomain: "ohmystand.firebaseapp.com",
-      projectId: "ohmystand",
-      storageBucket: "ohmystand.appspot.com",
-      messagingSenderId: "137154397242",
-      appId: "1:137154397242:web:d85c99e2ae42272e4ba103",
-      measurementId: "G-T4778Z0PPC"
-    })),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
   ]
 };
